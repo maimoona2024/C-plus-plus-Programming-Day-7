e5ec1ebc02a1a2056f32c732f6b4c2f891489ae8
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-10 23:15:32 +0500

    Add files via upload

---
+++ Day7.cpp
@@ -0,0 +1,25 @@
+//Program # 1: Typecasting
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int x;
+//     cout<<"Please! Enter a number: ";
+//     cin>>x;
+//     char y = (char)x; //Typecasting.
+//     cout<<y;
+// }
+
+//Program # 2. 
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     cout<<3/7<<endl; //integer/integer = integer.
+//     cout<<3.0/7<<endl; //float/integer = float.
+//     cout<<3/7.0<<endl; //integer/float = float.
+//     cout<<3.0/7.0<<endl; //float/float = float.
+// }
+
+
